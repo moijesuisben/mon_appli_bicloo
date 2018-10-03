b40ed6d3b7404902d544d350564706b4c0d96c45
Benjamin Arpage <benji.apg@gmail.com>
2018-10-03 15:24:38 +0200

    Modif js

--- script/free_bikes.js
+++ script/free_bikes.js
@@ -5,16 +5,14 @@ fetch('https://api.citybik.es/v2/networks/bicloo').then(function(res) {
       var stations = data.network.stations;
 
       for (var i = 0; i < stations.length; i++) {
-        var maListe2 = document.createElement('li')
+        if (stations[i].free_bikes !== 0) {
+          var maListe2 = document.createElement('li')
 
-        maListe2.textContent = (stations[i].name + ' ( ' + stations[i].extra.address + ' ) : places disponibles : ' +
-        stations[i].empty_slots + ' // vélo disponibles : ' + stations[i].free_bikes);
+          maListe2.textContent = (stations[i].name + ' ( ' + stations[i].extra.address + ' ) : places disponibles : ' +
+          stations[i].empty_slots + ' // vélo disponibles : ' + stations[i].free_bikes);
 
-        monUl.appendChild(maListe2);
-
-       if (stations[i].empty_slots = 0) {
-           where.classList.add.monUl('none');
-         }
+          monUl.appendChild(maListe2);
+        }
     }
 
   })
--- script/free_stations.js
+++ script/free_stations.js
@@ -5,18 +5,15 @@ fetch('https://api.citybik.es/v2/networks/bicloo').then(function(res) {
       var stations = data.network.stations;
 
       for (var i = 0; i < stations.length; i++) {
-        var maListe2 = document.createElement('li')
+        if (stations[i].empty_slots !== 0) {
+          var maListe2 = document.createElement('li')
 
-        maListe2.textContent = (stations[i].name + ' ( ' + stations[i].extra.address + ' ) : places disponibles : ' +
-        stations[i].empty_slots + ' // vélo disponibles : ' + stations[i].free_bikes);
+          maListe2.textContent = (stations[i].name + ' ( ' + stations[i].extra.address + ' ) : places disponibles : ' +
+          stations[i].empty_slots + ' // vélo disponibles : ' + stations[i].free_bikes);
 
-        monUl.appendChild(maListe2);
-
-        if (stations[i].free_bikes < 0) {
-          where.classList.add('rouge');
+          monUl.appendChild(maListe2);
         }
     }
-
 
   })
 })
